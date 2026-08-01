476a09b576cda392da814b13aea975fb969b2c8c
.gitMohamed <mohamedfathidev161@gmail.com>
2026-08-01 14:17:03 +0300

    local-test for scanners

--- src/OrderApp/obj/Debug/net8.0/OrderApp.AssemblyInfo.cs
+++ src/OrderApp/obj/Debug/net8.0/OrderApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrderApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f494a3935e48cf0189e59437e36b3e58719a89")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrderApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrderApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
